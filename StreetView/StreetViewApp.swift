dd580e528c38697aca76f782dce7dc7c291a98ca
Jacek Kosiński <kashiash@gmail.com>
2023-05-10 23:47:11 +0200

    Initial Commit

---
+++ StreetView/StreetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetViewApp.swift
+//  StreetView
+//
+//  Created by Jacek Kosinski U on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StreetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
